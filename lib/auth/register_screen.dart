2abe597ac2363ee1bf7b1c9bb929ea80ecb73a66
Alstonishere <alston.wongyongyi@gmail.com>
2026-08-21 15:41:55 +0800

    Initial Commit

---
+++ lib/auth/register_screen.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import '../models/user_model.dart';
+
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({super.key});
+
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  final _formKey = GlobalKey<FormState>();
+  
+  final TextEditingController usernameCtrl = TextEditingController();
+  final TextEditingController fullnameCtrl = TextEditingController();
+  final TextEditingController passwordCtrl = TextEditingController();
+  
+  String? _validatePassword(String? value){
+    if (value == null || value.isEmpty){
+      return 'Password is required';
+    }
+    if(value.length < 8){
+      return  'Password must at least 8 characters long';
+    }
+    if(!value.contains(RegExp(r'[A-Z]'))){
+      return 'Must contain at least 1 uppercase letter';
+    }
+    if(!value.contains(RegExp(r'a-z'))){
+      return 'Must contain at least 1 lower case';
+    }
+    if(!value.contains(RegExp(r'[\\!@#$&*~%^().,]'))){
+      return 'Must return at least 1 special character';
+    }
+    return null;
+  }
+  
+  void _register(){
+    if(_formKey.currentState!.validate()){
+      mockUserDatabase.add(
+        AppUser(
+            username: usernameCtrl.text.trim(), 
+            fullName: fullnameCtrl.text.trim(), 
+            password: passwordCtrl.text.trim(), 
+            role: 'User' // always assigned as User in Global Registration
+        )
+      );
+      
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+            content: Text('Registration Successfully')
+        )
+      );
+      Navigator.pop(context);
+    }
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('User Registration')
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+            child: ListView(
+              children: [
+                TextFormField(
+                  controller: usernameCtrl,
+                  decoration: const InputDecoration(labelText: 'Username',hint: Text('enter username here')),
+                  validator: (value){
+                    if (value == null || value.isEmpty){
+                      return 'Do not leave blank';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 12,),
+                TextFormField(
+                  controller: fullnameCtrl,
+                  decoration: const InputDecoration(labelText: 'Full Name',hint: Text('enter name here')),
+                  validator: (value){
+                    if (value == null || value.isEmpty){
+                      return 'Do not leave blank';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 12,),
+                TextFormField(
+                  controller: passwordCtrl,
+                  decoration: const InputDecoration(labelText: 'Password',hint: Text('enter password here')),
+                  validator: _validatePassword,
+                  obscureText: true,
+                ),
+                const SizedBox(height: 12,),
+                ElevatedButton(
+                    onPressed: _register, 
+                    child: const Text('Register Now')
+                )
+              ],
+            )
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/models/user_model.dart
@@ -0,0 +1,28 @@
+class AppUser{
+  final String username;
+  final String fullName;
+  final String password;
+  final String role;
+
+  AppUser({
+    required this.username,
+    required this.fullName,
+    required this.password,
+    required this.role
+  });
+}
+
+List<AppUser> mockUserDatabase = [
+  AppUser(
+      username: 'admin',
+      fullName: 'Booi Ah Heng',
+      password: 'Admin12345@',
+      role: 'Staff'
+  ),
+  AppUser(
+      username: 'User',
+      fullName: 'Ger Ah Heng',
+      password: 'User12345@',
+      role: 'User'
+  )
+];
